51497f0de6cbe31f26fb815cb771520a2b1ab37a
unoptimal <unoptimalblogs@gmail.com>
2023-03-03 17:09:55 -0800

    test

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,19 @@
 <header>
-    <a href="/">unoptimal</a>
+    <a href="/"><img src="src\img\logo.png" alt="Logo"></a>
+    <a href="/essays">essays</a>
+    <a href="/videos">videos</a>
+    <a href="/code">code</a>
+    <a href="/misc">misc</a>
 </header> 
+
+<style>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
