865675330386786957ce9bf3d14eb482bdcf01d5
unoptimal <unoptimalblogs@gmail.com>
2023-03-03 16:46:02 -0800

    temporary social links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,6 @@ WEBSITE REBUILD PENDING
 <br>
 <br>
 <a href="https://www.youtube.com/@unoptimal">videos</a>
+<a href="https://twitter.com/thatsnotoptimal">twitter</a>
+<a href="https://github.com/unoptimal">github</a>
+
