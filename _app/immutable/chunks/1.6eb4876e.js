77d0fc46571f0845f9af89a6b7bad8dd5058a73f
unoptimal <unoptimalblogs@gmail.com>
2023-03-03 16:46:32 -0800

    Updates

--- _app/immutable/chunks/1.6eb4876e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.2692eb28.js";export{t as component};
--- _app/immutable/chunks/2.5da79c8d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.ab56c1e0.js";export{t as component};
