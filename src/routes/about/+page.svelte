15b0c3418b679538bad8230fdea68f15497830e3
unoptimal <unoptimalblogs@gmail.com>
2023-08-13 12:11:11 -0400

    b

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -16,12 +16,10 @@
 <h1>hi! i'm justin. i like making things.</h1>
 <img src="/me.PNG" alt="">
 
-<p>in particular: absurd, whimsical and playful things. the term i've adopted to describe this is
-    <a href='https://unoptimal.substack.com/p/an-introduction-to-backpage-content' target='_blank'>"backpage content"</a>
-    - if this website doesn't give you a good sense of what that means, i maintain a 
-    list of projects that i deem backpage-esque <a href='https://backpage-nine.vercel.app/' target='_blank'>here</a>.
+<p>in particular: absurd, whimsical and playful things. the term i've adopted from my high school newspaper in an attempt to describe this is
+    <a href='https://unoptimal.substack.com/p/an-introduction-to-backpage-content' target='_blank'>"backpage content"</a>. will report back when i figure out what that means.</p>
 
-<p>these days i usually find myself making interactive web content and trying to get into physical computing and design.
+<p>these days i usually find myself making videos and interactive web content while trying to get into physical computing and design.
     i don't really know what means, but it sounds legible enough.
 
 </p>
---
+++ src/routes/wider-pringles-cans/+page.svelte
@@ -0,0 +1,64 @@
+<script context="module">
+    export async function load({ page, fetch }) {
+        return {
+            props: {}
+        };
+    }
+</script>
+
+<svelte:head>
+    <title>Wider Pringles Cans</title>
+    <meta name="description" content="Never get your hand stuck in a Pringles can again.">
+</svelte:head>
+
+
+<div id='container'>
+    <h1>Wider Pringles Cans</h1>
+    <img src="/pringles2.webp" alt="" style='display:block'>
+    <img src="/pringles3.webp" alt="" style='display:block'>
+
+    <div style="position: relative; padding-bottom: 56.25%; padding-top: 30px; height: 0; margin: 0 auto;">
+        <iframe src="https://www.youtube.com/embed/4TBWZnceI9o" title="Introducing: Wider Pringles Cans" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; margin: 0;"></iframe>
+    </div>
+
+    <p>Never get your hand stuck inside of a Pringles can again.</p>
+    <p>View the website <a href='https://wider-pringles-cans.vercel.app/' target='_blank'>here</a>, and the video <a href='https://www.youtube.com/watch?v=4TBWZnceI9o' target='_blank'>here</a>.</p>
+</div>
+
+
+<style>
+    #container {
+        max-width: 800px;
+        margin: 0 auto;
+    }
+    h1 {
+        font-size: 2.75em;
+        
+    }
+    img {
+        width: 100%;
+    }
+    p {
+        font-size: 1.75em;
+    }
+    a{
+        color: #16b7ff;
+    }
+    @media (max-width: 720px) {
+        h1 {
+            font-size: 2em;
+            margin-left: 20px;
+        }
+        p {
+            margin: 10px 0;
+            padding: 0 20px;
+            font-size: 1.75em;
+        }
+        iframe {
+            width: 100%;
+            height: auto;
+            padding: 0 20px;
+        }
+    }
+
+</style>
