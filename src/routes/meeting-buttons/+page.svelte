eba00dee0573b112fa52dd5f9e1646adbc53771e
unoptimal <unoptimalblogs@gmail.com>
2023-09-27 11:17:36 -0700

    mm

---
+++ src/routes/meeting-buttons/+page.svelte
@@ -0,0 +1,56 @@
+<script context="module">
+    export async function load({ page, fetch }) {
+        return {
+            props: {}
+        };
+    }
+</script>
+
+<svelte:head>
+    <title>this meeting ends when most people decide it SUCKS</title>
+    <meta name="description" content="Minimize wasted time in terrible meetings with this new technology.">
+</svelte:head>
+
+
+<div id='container'>
+    <h1>this meeting ends when most people decide it SUCKS</h1>
+
+    <div style="position: relative; padding-bottom: 56.25%; padding-top: 30px; height: 0; margin: 0 auto;">
+        <iframe src="https://www.youtube.com/embed/L1zs8c32Kqs" title="this meeting ends when most people decide it SUCKS" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; margin: 0;"></iframe>
+    </div>
+
+    <p>Everyone in a meeting gets a button. If a majority press the button, the meeting ends.</p>
+    <p>Made with a handful of ESP8266's, buttons, and a lot of tape.</p>
+</div>
+
+
+<style>
+    #container {
+        max-width: 800px;
+        margin: 0 auto;
+    }
+    h1 {
+        font-size: 2.75em;
+        
+    }
+    p {
+        font-size: 1.75em;
+    }
+    @media (max-width: 720px) {
+        h1 {
+            font-size: 2em;
+            margin-left: 20px;
+        }
+        p {
+            margin: 10px 0;
+            padding: 0 20px;
+            font-size: 1.75em;
+        }
+        iframe {
+            width: 100%;
+            height: auto;
+            padding: 0 20px;
+        }
+    }
+
+</style>
