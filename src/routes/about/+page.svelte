d82df9e4995b2a90ff9c658806e046731615c52e
unoptimal <unoptimalblogs@gmail.com>
2023-10-28 13:03:15 -0700

    haha

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -17,15 +17,12 @@
 <img src="/me.PNG" alt="">
 
 <p>in particular: absurd, whimsical and playful things. the term i've adopted from my high school newspaper in an attempt to describe this is
-    <a href='https://unoptimal.substack.com/p/an-introduction-to-backpage-content' target='_blank'>"backpage content"</a>. will report back when i figure out what that means.</p>
+    <a href='https://unoptimal.substack.com/p/an-introduction-to-backpage-content' target='_blank'>"backpage content"</a>.</p>
 
-<p>these days i usually find myself dabbling with interactive web content while trying to get into physical computing and design.
-    i don't really know what means, but it sounds legible enough.
-
-</p>
+<p>these days i usually find myself dabbling with DIY electronics and interactive web content.</p>
 
 <p>previously, i wrote a lot of essays. most of them were bad. i've since moved the few i like to <a href='https://unoptimal.substack.com/' target='_blank'>substack</a>. 
-i also make videos from time to time. you can find them on <a href='https://www.youtube.com/@unoptimal' target='_blank'>my youtube channel</a>.</p>
+i also make videos from time to time. you can find them on <a href='https://www.youtube.com/@unoptimal' target='_blank'>my youtube channel</a>. i also save a lot of stuff on <a href='https://curius.app/unoptimal' target='_blank'>curius</a>.</p>
 
 <p>i like new friends. <a href='mailto:unoptimalblogs@gmail.com'>say hi!</a> (wherever you prefer)</p>
 </div>
---
+++ src/routes/screaming-backpack/+page.svelte
@@ -0,0 +1,64 @@
+<script context="module">
+    export async function load({ page, fetch }) {
+        return {
+            props: {}
+        };
+    }
+</script>
+
+<svelte:head>
+    <title>my backpack screams when someone else picks it up</title>
+    <meta name="description" content="Now THIS is truly a cutting edge anti-theft mechanism.">
+</svelte:head>
+
+
+<div id='container'>
+    <h1>my backpack screams when someone else picks it up</h1>
+    <blockquote class="tiktok-embed" cite="https://www.tiktok.com/@thatsnotoptimal/video/7295100575207705902" data-video-id="7295100575207705902" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@thatsnotoptimal" href="https://www.tiktok.com/@thatsnotoptimal?refer=embed">@thatsnotoptimal</a> <p>this is a cutting edge anti theft mechanism </p> <a target="_blank" title="♬ original sound - unoptimal" href="https://www.tiktok.com/music/original-sound-7295100614516951854?refer=embed">♬ original sound - unoptimal</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>
+    <img src="/backpack2.webp" alt="" style='display:block'>
+    <p>I don't work in public spaces because I always worry that someone is going to take my stuff. Here's my solution.</p>
+
+    
+</div>
+
+
+<style>
+    #container {
+        max-width: 800px;
+        margin: 0 auto;
+    }
+    h1 {
+        font-size: 2.75em;
+        
+    }
+    img {
+        width: 100%;
+    }
+    p {
+        font-size: 1.75em;
+    }
+    a{
+        color: #16b7ff;
+    }
+    @media (max-width: 720px) {
+        h1 {
+            font-size: 2em;
+            margin-left: 20px;
+        }
+        p {
+            margin: 10px 0;
+            padding: 0 20px;
+            font-size: 1.75em;
+        }
+        iframe {
+            width: 100%;
+            height: auto;
+            padding: 0 20px;
+        }
+        img{
+            padding: 0 20px;
+
+        }
+    }
+
+</style>
