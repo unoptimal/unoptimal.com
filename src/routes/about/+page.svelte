49063c0ab2ee330eb894e7cc8f030311c1052021
unoptimal <unoptimalblogs@gmail.com>
2023-11-14 16:30:31 -0800

    a

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -16,13 +16,9 @@
 <h1>hi! i'm justin. i like making things.</h1>
 <img src="/me.PNG" alt="">
 
-<p>in particular: absurd, whimsical and playful things. the term i've adopted from my high school newspaper in an attempt to describe this is
-    <a href='https://unoptimal.substack.com/p/an-introduction-to-backpage-content' target='_blank'>"backpage content"</a>.</p>
+<p>in particular: absurd, whimsical, and playful things. these days i usually find myself dabbling with DIY electronics and interactive web content.</p>
 
-<p>these days i usually find myself dabbling with DIY electronics and interactive web content.</p>
-
-<p>previously, i wrote a lot of essays. most of them were bad. i've since moved the few i like to <a href='https://unoptimal.substack.com/' target='_blank'>substack</a>. 
-i also make videos from time to time. you can find them on <a href='https://www.youtube.com/@unoptimal' target='_blank'>my youtube channel</a>. i also save a lot of stuff on <a href='https://curius.app/unoptimal' target='_blank'>curius</a>.</p>
+<p>previously, i wrote (and deleted) a lot of <a href='https://unoptimal.substack.com/' target='_blank'>essays</a>. from time to time i make <a href='https://www.youtube.com/@unoptimal' target='_blank'>videos</a>. i also save a lot of stuff on <a href='https://curius.app/unoptimal' target='_blank'>curius</a>.</p>
 
 <p>i like new friends. <a href='mailto:unoptimalblogs@gmail.com'>say hi!</a> (wherever you prefer)</p>
 </div>
